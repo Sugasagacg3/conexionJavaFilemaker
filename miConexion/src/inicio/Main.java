31079e2bc95c17e9349e60bdad08776c5c6df96d
Tu nombre <jose@masqueideas.com>
2024-01-21 21:29:38 +0100

    commit

---
+++ miConexion/src/inicio/Main.java
@@ -0,0 +1,12 @@
+package inicio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Funcionando");
+ConexionFilemkaer.conexionFilemaer();
+
+	}
+
+}
